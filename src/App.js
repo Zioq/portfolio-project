e1b933a5d4512b56ed8627045e6f05fa99635f77
Zioq <jr.roberthan@gmail.com>
2021-02-15 13:08:29 -0800

    Header complete

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Particles from 'react-particles-js';
 import NavBar from "./components/NavBar/NavBar";
 import Header from "./components/Header/Header";
 function App() {
   return (
     <>
+    <Particles 
+    className="particles-canvas"
+    params={{
+      particles: {
+        number: {
+          value: 30,
+          density: {
+            enabled: true,
+            value_area: 900
+          }
+        },
+        shape: {
+          type:"circle",
+          stroke: {
+            width:10,
+            color:"#f9ab00"
+          }
+        } 
+      }
+    }}
+
+    />
     <NavBar />
     <Header />
     </>
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,25 +2,30 @@ import React from "react";
 import Typed from "react-typed";
 
 const Header = () => {
-    return (
-        <div>
-            <div className="header-wraper">
-                <div className="main-info">
-                <br />
+  return (
+    <div>
+      <div className="header-wraper">
+        <div className="main-info">
+          <br />
 
-                    <h1>
-                        $cd ~/ "Explore" / "Jaehyeon Robert Han"
-                    </h1>
-                    <Typed className="typed-text" strings={["Web Development (MERN)","Software Engineering", "Algorithm", "Data Structure"]} 
-                        typeSpeed={40}
-                        backSpeed={60}
-                        loop
-                    />
-                </div>
-
-            </div>
+          <h1>$cd ~/ "Explore" / "Jaehyeon Robert Han"</h1>
+          <Typed
+            className="typed-text"
+            strings={[
+              "Web Development (MERN)",
+              "Software Engineering",
+              "Algorithm",
+              "Data Structure",
+            ]}
+            typeSpeed={40}
+            backSpeed={60}
+            loop
+          />
+         <a href="#" className="btn-main-offer">Contact Me</a>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
-export default Header
+export default Header;
